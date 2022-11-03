3f8756b64cbef639137aa1a75323f898f3f3c8ff
Nimje, Ankit <Ankit.Nimje@united.com>
2022-11-03 00:25:55 -0500

    install style library and add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "./styled";
+import StacklineLogo from "../../assets/StacklineLogo";
+
+export default function Header() {
+  return (
+    <Container>
+      <StacklineLogo />
+    </Container>
+  );
+}
---
+++ src/components/Header/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #0c2340;
+  padding: 5px 20px;
+`;
